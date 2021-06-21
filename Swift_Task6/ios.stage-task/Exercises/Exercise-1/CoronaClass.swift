dbbf13fb697dff46a2aeb941c93ed3ee0702367e
Maksim Kildishev <mkildishev@yandex.ru>
2021-06-21 21:28:44 +0300

    refactor: fold in function repeatable code

--- Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
@@ -28,29 +28,35 @@ extension CoronaClass {
         var candidatePair = Pair(first: 0, second: 0, pos: 0)
         if !seats.contains(0) {
             let firstElem = seats.first ?? 0
-            let newPair = Pair(first: -firstElem, second: firstElem, pos: 0)
-            if (newPair.prior > candidatePair.prior) {
-                candidatePair = newPair
+            if let newCandidate = makeNewCandidatePair(candidatePrior: candidatePair.prior, first: -firstElem, second: firstElem, pos: 0) {
+                candidatePair = newCandidate
             }
+            
         } else if !seats.contains(seatsCapacity - 1) {
             let lastElem = seats.last!
-            let newPair = Pair(first: lastElem, second: (seatsCapacity - 1) * 2 - lastElem , pos: seats.count)
-            if (newPair.prior > candidatePair.prior) {
-                candidatePair = newPair
+            if let newCandidate = makeNewCandidatePair(candidatePrior: candidatePair.prior, first: lastElem, second: (seatsCapacity - 1) * 2 - lastElem, pos: seats.count) {
+                candidatePair = newCandidate
             }
+            
         }
         if (seats.isEmpty || seats.count - 1 == 0) {
             return candidatePair
         }
         for i in 1...seats.count - 1 {
-            let newPair = Pair(first: seats[i-1], second: seats[i], pos: i)
-            if (newPair.prior > candidatePair.prior) {
-                candidatePair = newPair
+            if let newCandidate = makeNewCandidatePair(candidatePrior: candidatePair.prior, first: seats[i-1], second: seats[i], pos: i) {
+                candidatePair = newCandidate
             }
         }
         
-
         return candidatePair
+    }
+    
+    func makeNewCandidatePair(candidatePrior: Int, first: Int, second: Int, pos: Int) -> Pair? {
+        let newPair = Pair(first: first, second: second, pos: pos)
+        if (newPair.prior > candidatePrior) {
+            return newPair
+        }
+        return nil
     }
     
 }
